d7a43276e5039ad50579de61f1c442c0d392bc80
jose alejandro bolanos arroyave <jose.bolanos@correounivalle.edu.co>
2021-07-13 08:55:42 +0200

    feat: improve labileData function and add test cases

--- src/labileData.js
+++ src/labileData.js
@@ -1,27 +1,49 @@
 import {
-    getShortestPaths,
-    getGroupedDiastereotopicAtomIDs,
-    getPathsInfo
-  } from 'openchemlib-utils';
-import { labileFilter } from './labileFilter';
+  getGroupedDiastereotopicAtomIDs,
+  getShortestPaths,
+} from 'openchemlib-utils';
 
-export function labileData(molecule){
-    molecule.addImplicitHydrogens();
-    molecule.addMissingChirality();
-    let paths = getPathsInfo(molecule)
-    let labileData = {
-      labileProton: false,
-      labileProtonNumber: 0 ,
-      heteroatomsList : [null]
+export function labileData(molecule) {
+  const diaIDs = getGroupedDiastereotopicAtomIDs(molecule);
+  const connections = getShortestPaths(molecule, {
+    toLabel: 'H',
+    maxLength: 1,
+  });
+
+  let nbAllLabiles = 0;
+  let nbDiffLabiles = 0;
+
+  for (const diaID of diaIDs) {
+    diaID.isLabile = isItLabile(diaID, molecule, connections);
+    if (diaID.isLabile) {
+      nbAllLabiles += diaID.atoms.length;
+      nbDiffLabiles++;
     }
-    let heteroAtomsData = paths.filter(x => x.label != 'C' & x.label != 'H').filter(x => x.allHydrogens > 0)
-    if (heteroAtomsData.length > 0) {
-      labileData.labileProton = true;
-      labileData.labileProtonNumber = (heteroAtomsData.map(x => x.allHydrogens).reduce(function(a, b){
-        return a + b;
-    }, 0)),
-    labileData.heteroatomsList = heteroAtomsData.map(x => x.label);
-    }
-    
-  return labileData
+  }
+
+  return {
+    hasLabile: nbAllLabiles > 0,
+    nbDiffLabiles,
+    nbAllLabiles,
+    diaIDs,
+    connections,
+  };
+}
+
+function isItLabile(diaId, molecule, connections) {
+  if (diaId.atomLabel !== 'H') return false;
+
+  let connectedTo = connections[diaId.atoms[0]];
+  let path = connectedTo.find((p) => p && p.length > 1);
+  let atomLabel = molecule.getAtomLabel(path[1]);
+
+  switch (atomLabel) {
+    case 'N':
+    case 'O':
+    case 'Cl':
+    case 'S':
+      return true;
+    default:
+      return false;
+  }
 }
