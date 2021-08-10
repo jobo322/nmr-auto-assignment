c7af512a2bc238583f49065590ab00296a8ce06c
Jhon Jairo Bohorquez Santiago <jjsantiago@pop-os.localdomain>
2021-08-09 22:11:59 -0500

    add groupAromaticRanges function

---
+++ src/groupAromaticRanges.js
@@ -0,0 +1,20 @@
+export function groupAromaticRanges(ranges){
+    let aromaticRange = ranges.filter( x => (x.from > 6.0 & x.to < 8.0))
+    return joinRanges(aromaticRange)
+}
+function joinRanges(ranges){
+
+    let fromData = ranges.map(a => a.from).sort(function(a, b) {
+        return a - b;
+        });;
+    let toData = ranges.map(a => a.to).sort(function(a, b) {
+        return a - b;
+        });
+    let newRange = {
+        from : fromData[0],
+        to: toData[toData.length - 1],
+        integral : ranges.map(a => a.integral).reduce((a, b) => a + b, 0),
+        signal : ranges.map(a => a.signal)[0]
+    } 
+    return newRange
+}
